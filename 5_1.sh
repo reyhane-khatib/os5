505e8b971377fb853f4444928f03f909d9be14ba
reyhane-khatib <83624028+reyhane-khatib@users.noreply.github.com>
2021-05-06 05:54:30 +0430

    Delete 5_1.sh

--- 5_1.sh
+++
@@ -1,5 +0,0 @@
-reyhan@ubuntu:~$ git config --global user.name"reyhane-khatib"
-reyhan@ubuntu:~$ git config --global user.email "reyhanekhatibsemnani@gmail.com"
-reyhan@ubuntu:~$ git config --global credential.helper store
-reyhan@ubuntu:~$ git config --global credential.helper 'timeout=86400'
-
